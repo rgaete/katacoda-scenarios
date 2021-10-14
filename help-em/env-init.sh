cd3f6589f55ba84533277918d7feb622850b13f1
Ricardo Gaete <ricardo.gaete@walmart.com>
2021-10-14 19:11:21 -0300

    testE

--- help-em/env-init.sh
+++ help-em/env-init.sh
@@ -1,6 +1,6 @@
 curl -L https://github.com/katacoda/scenario-examples/blob/master/custom-events/site/site.tar.gz?raw=true > ~/site.tar.gz
-tar -xvzf ~/site.tar.gz -C /home/scrapbook/tutorial
-cd /home/scrapbook/tutorial
+tar -xvzf ~/site.tar.gz -C /root
+cd /root
 npm install
 echo "Starting... this will block the rest of the commands from running..."
 npm start
