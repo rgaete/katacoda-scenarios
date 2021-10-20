f772af7193672bf0b206af63ea39819caad03ff9
Ricardo Gaete <ricardo.gaete@walmart.com>
2021-10-20 10:32:10 -0300

    Adding code to make the exercise

--- help-em/env-init.sh
+++ help-em/env-init.sh
@@ -1,6 +1,7 @@
 curl -L https://github.com/katacoda/scenario-examples/blob/master/custom-events/site/site.tar.gz?raw=true > ~/site.tar.gz
 tar -xvzf ~/site.tar.gz -C /root
 cd /root
+curl -L https://github.com/rgaete/katacoda-scenarios/help-em/site/exercise.js > exercise.js
 npm install
 echo "Starting... this will block the rest of the commands from running..."
 npm start
