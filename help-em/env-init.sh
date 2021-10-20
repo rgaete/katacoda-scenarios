bb46367590afff540ca87f3a79b998d14d631c45
Ricardo Gaete <ricardo.gaete@walmart.com>
2021-10-20 12:03:45 -0300

    Adding images

--- help-em/env-init.sh
+++ help-em/env-init.sh
@@ -1,6 +1,5 @@
 curl -L https://github.com/katacoda/scenario-examples/blob/master/custom-events/site/site.tar.gz?raw=true > ~/site.tar.gz
 tar -xvzf ~/site.tar.gz -C /root
-cd /root
 curl -L https://github.com/rgaete/katacoda-scenarios/blob/main/help-em/site/exercise.js?raw=true > exercise.js
 npm install
 echo "Starting... this will block the rest of the commands from running..."
