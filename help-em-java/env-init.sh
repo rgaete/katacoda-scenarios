8769f5bf5a0b82cbf6e017bc7ec7a04e3e48a751
Ricardo Gaete <ricardo.gaete@walmart.com>
2021-10-28 16:02:40 -0300

    Adding more languages

--- help-em-java/env-init.sh
+++ help-em-java/env-init.sh
@@ -1,7 +1,3 @@
-curl -L https://github.com/katacoda/scenario-examples/blob/master/custom-events/site/site.tar.gz?raw=true > ~/site.tar.gz
-tar -xvzf ~/site.tar.gz -C /root
 cd /root
-curl -L https://github.com/rgaete/katacoda-scenarios/blob/main/help-em/site/exercise.js?raw=true > exercise.js
-npm install
+curl -L https://github.com/rgaete/katacoda-scenarios/blob/main/help-em-java/site/exercise.java?raw=true > exercise.java
 echo "Starting... this will block the rest of the commands from running..."
-npm start
