e100fcf319b267e5a036c60bc18143a052fbd0c5
Ricardo Gaete <ricardo.gaete@walmart.com>
2021-10-29 10:24:37 -0300

    Adding java

--- help-em-java/env-init.sh
+++ help-em-java/env-init.sh
@@ -1,4 +1,3 @@
-cd /root
 curl -L https://github.com/rgaete/katacoda-scenarios/blob/main/help-em-java/site/exercise.java?raw=true > exercise.java
 echo "Starting... this will block the rest of the commands from running..."
 java -version
