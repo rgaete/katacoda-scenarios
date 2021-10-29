9cd7f27b1a40579bfb8438de7ebfa3247b814105
Ricardo Gaete <ricardo.gaete@walmart.com>
2021-10-29 10:27:08 -0300

    Selecting right path

--- help-em-java/env-init.sh
+++ help-em-java/env-init.sh
@@ -1,3 +1,4 @@
+cd /home/scrapbook/tutorial
 curl -L https://github.com/rgaete/katacoda-scenarios/blob/main/help-em-java/site/exercise.java?raw=true > exercise.java
 echo "Starting... this will block the rest of the commands from running..."
 java -version
